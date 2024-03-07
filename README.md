337fe5041cc8040c34970db9e1235fa1c2e4b33b
brian <brian.aguilar235@gmail.com>
2024-03-07 14:08:16 -0800

    finally got screencast up

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# readme-generator
+
+ # readme generator 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description 
+ quick readme generator for projects 
+ https://drive.google.com/file/d/1LJhgJRXjKn3MuVwnx-x5ZhrHDmhFwKQS/view
+## Table of Contents
+- [Installation](#installation) 
+- [Usage](#usage) 
+- [Contributing](#contributing) 
+- [Tests](#tests)
+- [License](#license) 
+- [Questions](#questions) 
+## Installation 
+  follow prompts 
+  
+## Usage 
+   
+  
+## Contributing 
+   
+  
+## Tests 
+  follow prompts provided on terminal 
+  
+## License
+   This application is covered under the MIT license. 
+   
+## Questions For questions or inquiries, please contact: 
+- GitHub: [(https://github.com/bswagy)](https://github.com/(https://github.com/bswagy)) 
+- Email: brian.aguilar235@gmail.com 
+ 
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,4 +1,20 @@
+const renderBatch = (license) => {
+    if (license==="MIT") {
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    }
 
+   if (license==="Apache") {
+       return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+       }
+
+     if (license==="GPL") {
+        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+        }
+     if (license==="none") {
+       return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+            }
+
+}
 
 // generateMarkdown.js 
 
@@ -6,37 +22,36 @@
  const generateMarkdown = (data) => { 
 return `
  # ${data.title} 
-
- ## Description 
+${renderBatch(data.license)}
+## Description 
  ${data.description} 
  
- ## Table of Contents
- - [Installation](#installation) 
- - [Usage](#usage) 
- - [Contributing](#contributing) 
- - [Tests](#tests)
-  - [License](#license) 
-  - [Questions](#questions) 
-  
-  ## Installation 
+## Table of Contents
+- [Installation](#installation) 
+- [Usage](#usage) 
+- [Contributing](#contributing) 
+- [Tests](#tests)
+- [License](#license) 
+- [Questions](#questions) 
+## Installation 
   ${data.installation} 
   
-  ## Usage 
+## Usage 
   ${data.usage} 
   
-  ## Contributing 
+## Contributing 
   ${data.contributing} 
   
-  ## Tests 
+## Tests 
   ${data.test} 
   
-  ## License
+## License
    This application is covered under the ${data.license} license. 
    
-   ## Questions For questions or inquiries, please contact: 
-   - GitHub: [${data.github}](https://github.com/${data.github}) 
-   - Email: ${data.email} 
-   `; 
+## Questions For questions or inquiries, please contact: 
+- GitHub: [${data.github}](https://github.com/${data.github}) 
+- Email: ${data.email} 
+ `; 
 }; 
 
 module.exports = generateMarkdown;
